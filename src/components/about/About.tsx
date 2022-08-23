41740f1fa58f4b100d9ee474c94413d154f9d6a7
Ruben-sanz <rubendse1343@gmail.com>
2022-08-23 11:00:01 -0400

    About successfuly

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,10 +1,50 @@
-import React from 'react'
-import './about.css'
+import React from "react";
+import "./about.css";
+import ME from "../../assets/me-about.jpg";
+import {FaAward, FaUsers, FaFolderOpen} from 'react-icons/fa'
 
 const About = () => {
   return (
-    <section id='about'>About</section>
-  )
-}
+    <section id="about">
+      <h5>Necesitas Saber</h5>
+      <h2>Sobre Mi</h2>
 
-export default About
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About-img" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            {/* =========card con expeciencias=========== */}
+            <article className="about__card">
+              <FaAward className="about__icon"/>
+              <h5>Experiencia</h5>
+              <small>+6 meses Trabajando</small>
+            </article>
+
+            <article className="about__card">
+              <FaUsers className="about__icon"/>
+              <h5>Clientes</h5>
+              <small>10+ Worldwide</small>
+            </article>
+
+            <article className="about__card">
+              <FaFolderOpen className="about__icon"/>
+              <h5>Projectos</h5>
+              <small>+2 Completados</small>
+            </article>
+          </div>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem reiciendis aspernatur quo tempora. Sequi eum rem quis. Ea perferendis dolor repudiandae voluptatibus quaerat aliquid nam, eius esse recusandae, ipsum porro.</p>
+
+            <a href="#contact" className="btn btn-primary">Contactame</a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,17 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {AiOutlineHome, AiOutlineUser} from 'react-icons/ai';
 import {FiBook} from 'react-icons/fi';
 import {RiMessage2Line, RiServiceLine} from 'react-icons/ri';
 import './nav.css'
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
+
+ 
   return (
     <nav>
-      <a href="#"><AiOutlineHome/></a>
-      <a href="#about"><AiOutlineUser/></a>
-      <a href="#experience"><FiBook /></a>
-      <a href="#services"><RiServiceLine /></a>
-      <a href="#contact"><RiMessage2Line /></a>
+      <a href="#" onClick={()=> setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome/></a>
+      <a href="#about" onClick={()=> setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser/></a>
+      <a href="#experience" onClick={()=> setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><FiBook /></a>
+      <a href="#services" onClick={()=> setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><RiServiceLine /></a>
+      <a href="#contact" onClick={()=> setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><RiMessage2Line /></a>
     </nav>
   )
 }
